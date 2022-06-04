1a599bd36ee1307ff70135c47f0f006796281cf3
Bryan Bosire <bryanbosire@gmail.com>
2022-06-04 11:44:32 +0300

    commit tree 74e7d68c441ffa6a3bf91af0897b276479207923

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ function deleteFromEmployeeByKey(employee, key){
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key]
-    return employee
+    delete employee[key];
+    return employee;
 }
